5c218c711838ff35559d48f6adc02ae4a8c8a56a
caz002 <caz002@ucsd.edu>
2025-09-23 10:50:44 -0700

    added embeddings to data, fixed linting issues

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+"""Streamlit app for the PDF reader chatbot"""
 from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 def main():
+    """
+    This function starts the app, transforms the pdf data into embeddings, and creates the chatbot.
+    """
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF")
@@ -16,8 +23,20 @@ def main():
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
-        st.write(text)
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator = "\n",
+            chunk_size = 1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        st.write(chunks)
 
 if __name__ == '__main__':
     main()
-    
